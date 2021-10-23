b51cd36c172f1334c0810267823d4ad4a865b7f0
MrTofuuu <84428685+MrTofuuu@users.noreply.github.com>
2021-10-22 22:15:46 -0500

    added wildcard route to redirect back to index.html

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const path = require('path');
+const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Import custom middleware, "cLog"
+app.use(clog);
 
 // Middleware for parsing JSON and urlencoded form data 
 app.use(express.json());
@@ -21,6 +25,11 @@ app.get('/', (req, res) =>
 // GET Route for notes page
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, '/public/assets/notes.html'))
+);
+
+// Wildcard Route for homepage
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/assets/index.html'))
 );
 
 // listener 
